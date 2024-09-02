75ba3bd2aaad669601c303bd1dcf4df788f90290
Mário Lucas <mariolucasdev@gmail.com>
2024-09-01 21:48:50 -0300

    feat: adding ui and create and delete messages

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,22 +1,40 @@
+'use client'
+
 import { useState } from 'react'
-import Form from 'next/form'
 
 export default function Chat() {
-  return (
-    <div>
-      <main>
+    const [messages, setMessages] = useState([])
 
+    const send = (e) => {
+        e.preventDefault()
 
+        const message = e.target.message.value
+        e.target.message.value = ''
 
+        setMessages([...messages, message])
+    }
 
-        <Form action="/search">
-          {/* On submission, the input value will be appended to 
-          the URL, e.g. /search?query=abc */}
-          <input name="message" />
-          <button type="submit">Submit</button>
-        </Form>
-      </main>
+    const deleteMessage = (index) => {
+        setMessages(messages.filter((_, i) => i !== index))
+    }
 
-    </div>
-  )
+    return (
+        <div className="absolute p-5 h-full w-full mx-auto bg-indigo-500 flex justify-center">
+            <div className="w-3/4 p-5 rounded-md mx-auto bg-slate-100">
+                {messages.map((message, index) => (
+                    <div key={index} className="p-2 bg-white rounded-md mb-2">
+                        {message}
+                        <button className="float-right" onClick={() => deleteMessage(index)}>❌</button>
+                    </div>
+                ))}
+            </div>
+
+            <form className="absolute rounded-md bottom-2 w-3/4 p-8" onSubmit={send}>
+                <textarea placeholder='Digite sua mensagem 📰' autoFocus className='w-full text-2-lg p-4 rounded-md mb-2' name="message" />
+                <button
+                    className='w-full text-2-lg p-4 font-semibold rounded-md bg-indigo-500 text-white'
+                    type="submit"> Enviar ✈️</button>
+            </form>
+        </div>
+    )
 }
